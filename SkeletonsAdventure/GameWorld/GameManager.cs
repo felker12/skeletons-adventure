2bf66182348145336a71e17f0ee5b892993351c1
Anthony Felker <kanthonyfelker@hotmail.com>
2025-03-01 03:54:07 -0500

    test2

--- SkeletonsAdventure/GameWorld/GameManager.cs
+++ SkeletonsAdventure/GameWorld/GameManager.cs
@@ -1,5 +1,4 @@
-﻿using Assimp;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using RpgLibrary.EntityClasses;
